af4a01d0e25578682c86e2283d82003cd45420e8
figofuture@gmail.com <figofuture@gmail.com@55e3b4eb-e822-e86e-b1e7-3a08106dd6d7>
2012-12-18 07:26:54 +0000

    add power profile reader

---
+++ trunk/powerprofilereader/src/com/figo/testpowerprofile/MainActivity.java
@@ -0,0 +1,132 @@
+package com.figo.testpowerprofile;
+
+import org.apache.http.protocol.HTTP;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import com.figo.powerprofilereader.R;
+
+public class MainActivity extends Activity {
+
+	private static final String LOG_TAG = "MainActivity";
+	PowerProfile mPP;
+	EditText mET;
+	String mTitle;
+
+	String[] mTypes = { PowerProfile.POWER_SCREEN_ON,
+			PowerProfile.POWER_SCREEN_FULL, PowerProfile.POWER_BLUETOOTH_ON,
+			PowerProfile.POWER_BLUETOOTH_AT_CMD,
+			PowerProfile.POWER_BLUETOOTH_ACTIVE, PowerProfile.POWER_WIFI_ON,
+			PowerProfile.POWER_WIFI_SCAN, PowerProfile.POWER_WIFI_ACTIVE,
+			PowerProfile.POWER_AUDIO, PowerProfile.POWER_VIDEO,
+			PowerProfile.POWER_RADIO_ON, PowerProfile.POWER_RADIO_SCANNING,
+			PowerProfile.POWER_RADIO_ACTIVE, PowerProfile.POWER_GPS_ON,
+			PowerProfile.POWER_CPU_IDLE, PowerProfile.POWER_CPU_AWAKE,
+			PowerProfile.POWER_CPU_SPEEDS, PowerProfile.POWER_CPU_ACTIVE,
+			PowerProfile.POWER_BATTERY_CAPACITY, };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mTitle = Build.MANUFACTURER + "/" + Build.MODEL + "'s ";
+		mTitle += getResources().getString(R.string.share_subject);
+
+		this.setTitle(mTitle);
+
+		mET = (EditText) findViewById(R.id.content);
+
+		String device = "Board: " + Build.BOARD + "\n" + "Bootloader: "
+				+ Build.BOOTLOADER + "\n" + "Brand: " + Build.BRAND + "\n"
+				+ "CPU abi: " + Build.CPU_ABI + "\n" + "CPU abi2: "
+				+ Build.CPU_ABI2 + "\n" + "Device: " + Build.DEVICE + "\n"
+				+ "Display: " + Build.DISPLAY + "\n" + "Fingerprint: "
+				+ Build.FINGERPRINT + "\n" + "Hardware: " + Build.HARDWARE
+				+ "\n" + "Host: " + Build.HOST + "\n" + "Id: " + Build.ID
+				+ "\n" + "Manufacturer: " + Build.MANUFACTURER + "\n"
+				+ "Model: " + Build.MODEL + "\n" + "Product: " + Build.PRODUCT
+				+ "\n" + "Radio: " + Build.RADIO + "\n" + "Tags: " + Build.TAGS
+				+ "\n" + "Time: " + Build.TIME + "\n" + "Type:" + Build.TYPE
+				+ "\n" + "User: " + Build.USER + "\n"
+				+ "///////////////////////////////////////" + "\n";
+		Log.i(LOG_TAG, device);
+		mET.setText(device);
+
+		mPP = new PowerProfile(this);
+
+		double value = 0;
+		for (int i = 0; i < mTypes.length; i++) {
+			if (PowerProfile.POWER_RADIO_ON.equalsIgnoreCase(mTypes[i])) {
+				final int bins = 5;
+				for (int j = 0; j < bins; j++) {
+					value = mPP.getAveragePower(mTypes[i], j);
+					String text = mTypes[i] + " level " + j + ": " + value
+							+ "\n";
+					Log.i(LOG_TAG, text);
+					mET.setText(mET.getText() + text);
+				}
+			} else if (PowerProfile.POWER_CPU_SPEEDS
+					.equalsIgnoreCase(mTypes[i])) {
+				final int speedSteps = mPP.getNumSpeedSteps();
+				for (int j = 0; j < speedSteps; j++) {
+					value = mPP.getAveragePower(mTypes[i], j);
+					String text = mTypes[i] + " level " + j + ": " + value
+							+ "\n";
+					Log.i(LOG_TAG, text);
+					mET.setText(mET.getText() + text);
+				}
+			} else if (PowerProfile.POWER_CPU_ACTIVE
+					.equalsIgnoreCase(mTypes[i])) {
+				final int speedSteps = mPP.getNumSpeedSteps();
+				for (int j = 0; j < speedSteps; j++) {
+					value = mPP.getAveragePower(mTypes[i], j);
+					String text = mTypes[i] + " level " + j + ": " + value
+							+ "\n";
+					Log.i(LOG_TAG, text);
+					mET.setText(mET.getText() + text);
+				}
+			} else {
+				value = mPP.getAveragePower(mTypes[i]);
+				String text = mTypes[i] + ": " + value + "\n";
+				Log.i(LOG_TAG, text);
+				mET.setText(mET.getText() + text);
+			}
+
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_share:
+			share();
+			return true;
+		}
+		return false;
+	}
+
+	private void share() {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType(HTTP.PLAIN_TEXT_TYPE);
+		String[] email = { "", };
+		intent.putExtra(Intent.EXTRA_EMAIL, email);
+		intent.putExtra(Intent.EXTRA_SUBJECT, mTitle);
+		intent.putExtra(Intent.EXTRA_TEXT, mET.getText());
+		startActivity(Intent.createChooser(intent, mTitle));
+	}
+
+}
---
+++ trunk/powerprofilereader/src/com/figo/testpowerprofile/PowerProfile.java
@@ -0,0 +1,215 @@
+package com.figo.testpowerprofile;
+
+import android.content.Context;
+import android.util.Log;
+
+public class PowerProfile {
+	static final String LOG_TAG = PowerProfile.class.getSimpleName();
+	static final Class<?> CLASS;
+	static {
+		Class<?> klass = null;
+		try {
+			klass = Class.forName("com.android.internal.os.PowerProfile");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+			klass = null;
+		} finally {
+			CLASS = klass;
+		}
+	}
+
+	final Object mThis;
+
+	/**
+	 * No power consumption, or accounted for elsewhere.
+	 */
+	public static final String POWER_NONE = "none";
+
+	/**
+	 * Power consumption when CPU is in power collapse mode.
+	 */
+	public static final String POWER_CPU_IDLE = "cpu.idle";
+
+	/**
+	 * Power consumption when CPU is awake (when a wake lock is held). This
+	 * should be 0 on devices that can go into full CPU power collapse even when
+	 * a wake lock is held. Otherwise, this is the power consumption in addition
+	 * to POWERR_CPU_IDLE due to a wake lock being held but with no CPU
+	 * activity.
+	 */
+	public static final String POWER_CPU_AWAKE = "cpu.awake";
+
+	/**
+	 * Power consumption when CPU is in power collapse mode.
+	 */
+	public static final String POWER_CPU_ACTIVE = "cpu.active";
+
+	/**
+	 * Power consumption when WiFi driver is scanning for networks.
+	 */
+	public static final String POWER_WIFI_SCAN = "wifi.scan";
+
+	/**
+	 * Power consumption when WiFi driver is on.
+	 */
+	public static final String POWER_WIFI_ON = "wifi.on";
+
+	/**
+	 * Power consumption when WiFi driver is transmitting/receiving.
+	 */
+	public static final String POWER_WIFI_ACTIVE = "wifi.active";
+
+	/**
+	 * Power consumption when GPS is on.
+	 */
+	public static final String POWER_GPS_ON = "gps.on";
+
+	/**
+	 * Power consumption when Bluetooth driver is on.
+	 */
+	public static final String POWER_BLUETOOTH_ON = "bluetooth.on";
+
+	/**
+	 * Power consumption when Bluetooth driver is transmitting/receiving.
+	 */
+	public static final String POWER_BLUETOOTH_ACTIVE = "bluetooth.active";
+
+	/**
+	 * Power consumption when Bluetooth driver gets an AT command.
+	 */
+	public static final String POWER_BLUETOOTH_AT_CMD = "bluetooth.at";
+
+	/**
+	 * Power consumption when screen is on, not including the backlight power.
+	 */
+	public static final String POWER_SCREEN_ON = "screen.on";
+
+	/**
+	 * Power consumption when cell radio is on but not on a call.
+	 */
+	public static final String POWER_RADIO_ON = "radio.on";
+
+	/**
+	 * Power consumption when cell radio is hunting for a signal.
+	 */
+	public static final String POWER_RADIO_SCANNING = "radio.scanning";
+
+	/**
+	 * Power consumption when talking on the phone.
+	 */
+	public static final String POWER_RADIO_ACTIVE = "radio.active";
+
+	/**
+	 * Power consumption at full backlight brightness. If the backlight is at
+	 * 50% brightness, then this should be multiplied by 0.5
+	 */
+	public static final String POWER_SCREEN_FULL = "screen.full";
+
+	/**
+	 * Power consumed by the audio hardware when playing back audio content.
+	 * This is in addition to the CPU power, probably due to a DSP and / or
+	 * amplifier.
+	 */
+	public static final String POWER_AUDIO = "dsp.audio";
+
+	/**
+	 * Power consumed by any media hardware when playing back video content.
+	 * This is in addition to the CPU power, probably due to a DSP.
+	 */
+	public static final String POWER_VIDEO = "dsp.video";
+
+	public static final String POWER_CPU_SPEEDS = "cpu.speeds";
+
+	/**
+	 * Battery capacity in milliAmpHour (mAh).
+	 */
+	public static final String POWER_BATTERY_CAPACITY = "battery.capacity";
+
+	public PowerProfile(Context context) {
+		Object powerProfile = null;
+		try {
+			powerProfile = CLASS.getConstructor(Context.class).newInstance(
+					context);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+		} finally {
+			mThis = powerProfile;
+		}
+	}
+
+	/**
+	 * Returns the average current in mA consumed by the subsystem
+	 * 
+	 * @param type
+	 *            the subsystem type
+	 * @return the average current in milliAmps.
+	 */
+	public double getAveragePower(String type) {
+		try {
+			return (Double) CLASS.getMethod("getAveragePower", String.class)
+					.invoke(mThis, type);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+		}
+		return 0;
+	}
+
+	/**
+	 * Returns the average current in mA consumed by the subsystem for the given
+	 * level.
+	 * 
+	 * @param type
+	 *            the subsystem type
+	 * @param level
+	 *            the level of power at which the subsystem is running. For
+	 *            instance, the signal strength of the cell network between 0
+	 *            and 4 (if there are 4 bars max.) If there is no data for
+	 *            multiple levels, the level is ignored.
+	 * @return the average current in milliAmps.
+	 */
+	public double getAveragePower(String type, int level) {
+		try {
+			return (Double) CLASS.getMethod("getAveragePower", String.class,
+					Integer.TYPE).invoke(mThis, type, level);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+		}
+		return 0;
+	}
+
+	/**
+	 * Returns the number of speeds that the CPU can be run at.
+	 * 
+	 * @return
+	 */
+	public int getNumSpeedSteps() {
+		try {
+			return (Integer) CLASS.getMethod("getNumSpeedSteps").invoke(mThis);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+		}
+		return 0;
+	}
+
+	/**
+	 * Returns the battery capacity, if available, in milli Amp Hours. If not
+	 * available, it returns zero.
+	 * 
+	 * @return the battery capacity in mAh
+	 */
+	public double getBatteryCapacity() {
+		try {
+			return (Double) CLASS.getMethod("getBatteryCapacity").invoke(mThis);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(LOG_TAG, e.getMessage());
+		}
+		return 0;
+	}
+
+}
